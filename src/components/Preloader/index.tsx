13cfff1b3ebd8022361b39f6a106436ddbe847c0
David Engel <davidengel@Davids-MacBook-Pro.local>
2022-08-23 20:55:28 -0700

    Add scroll to start

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -1,7 +1,36 @@
 import { FC, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import { Box, BoxProps, Flex, Text, VStack } from '@chakra-ui/react'
+import { Box, BoxProps, Flex, Icon, Text, VStack } from '@chakra-ui/react'
+import { FiArrowDown } from 'react-icons/fi'
 import styles from './styles.module.css'
+
+const ScrollInstructions: FC = () => {
+  return (
+    <Flex
+      w={'full'}
+      className={'animate__animated animate__fadeInUpBig ' + styles.instructions}
+      alignItems={'center'}
+      justifyContent={'center'}>
+      <VStack
+        alignItems={'center'}
+        justifyContent={'center'}
+        p={1}
+        spacing={0}
+        className={'animate__animated animate__infinite animate__pulse'}>
+        <Text fontSize={'4xl'} color={'blue.400'} mb={6} textShadow={'1px 1px rgba(0, 0, 0, 0.09)'}>
+          <strong>Scroll down to start!</strong>
+        </Text>
+        <Icon
+          as={FiArrowDown}
+          height={16}
+          width={16}
+          color={'white'}
+          textShadow={'1px 1px rgba(0, 0, 0, 0.09)'}
+        />
+      </VStack>
+    </Flex>
+  )
+}
 
 type Props = BoxProps & {
   isLoading: boolean
@@ -10,6 +39,8 @@ type Props = BoxProps & {
 
 const Preloader: FC<Props> = ({ isLoading, setIsLoading, ...rest }: Props) => {
   const [isPreloading, setIsPreloading] = useState(true)
+  const [isInstructing, setIsInstructing] = useState(false)
+  const [position, setPosition] = useState(1)
   const router = useRouter()
 
   useEffect(() => {
@@ -20,11 +51,23 @@ const Preloader: FC<Props> = ({ isLoading, setIsLoading, ...rest }: Props) => {
       } else {
         setTimeout(() => {
           setIsPreloading(false)
-          setIsLoading(false)
+          setTimeout(() => {
+            setPosition(2)
+            setIsInstructing(true)
+          }, 800)
         }, 1000)
       }
     }
   }, [isPreloading, setIsPreloading, setIsLoading, router])
+
+  // Start on scroll
+  const handleScroll = () => {
+    setIsInstructing(false)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 800)
+  }
 
   return (
     <Box
@@ -34,15 +77,31 @@ const Preloader: FC<Props> = ({ isLoading, setIsLoading, ...rest }: Props) => {
       left={0}
       width={'full'}
       height={'100vh'}
+      overflowY={isInstructing ? 'scroll' : 'hidden'}
+      onScroll={handleScroll}
       bg={'black'}
       className={(!isLoading && 'animate__animated animate__slideOutDown') || ''}>
-      <Flex h={'full'} w={'full'} alignItems={'center'} justifyContent={'center'}>
-        <VStack className={(!isPreloading && 'animate__animated animate__backOutDown') || ''}>
-          <Flex className={styles.preloader} />
-          <Text fontSize={'2xl'} color={'white'}>
-            Loading...
-          </Text>
-        </VStack>
+      <Flex
+        w={'full'}
+        height={isInstructing ? '110vh' : 'full'}
+        alignItems={'center'}
+        justifyContent={'center'}>
+        {(position == 1 && (
+          <VStack className={(!isPreloading && 'animate__animated animate__backOutDown') || ''}>
+            <Flex className={styles.preloader} />
+            <Text fontSize={'2xl'} color={'white'}>
+              <strong>Loading...</strong>
+            </Text>
+          </VStack>
+        )) || (
+          <VStack
+            className={
+              (isInstructing && 'animate__animated animate__backInUp') ||
+              'animate__animated animate__backOutDown'
+            }>
+            <ScrollInstructions />
+          </VStack>
+        )}
       </Flex>
     </Box>
   )
