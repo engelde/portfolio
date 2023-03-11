111c9b5a81d27072f959a3b4b352b1299fedc878
David Engel <davidengel@live.com>
2023-03-11 08:30:40 -0800

    Adjust preload image quality

--- src/components/SuperMario/Landscape/Ground/index.tsx
+++ src/components/SuperMario/Landscape/Ground/index.tsx
@@ -15,7 +15,7 @@ const Ground: FC<Props> = ({ x, height, width }: Props) => {
           w={'12px'}
           h={height + 'px'}
           bg={
-            'url("/_next/image?url=%2Fimages%2Fground%2Fground.1.png&w=256&q=80") no-repeat left top'
+            'url("/_next/image?url=%2Fimages%2Fground%2Fground.1.png&w=256&q=75") no-repeat left top'
           }
           backgroundSize={'12px 128px'}
           overflow={'hidden'}
@@ -24,7 +24,7 @@ const Ground: FC<Props> = ({ x, height, width }: Props) => {
           w={width - 16 + 'px'}
           h={height + 'px'}
           bg={
-            'url("/_next/image?url=%2Fimages%2Fground%2Fground.2.png&w=256&q=80") repeat-x left top'
+            'url("/_next/image?url=%2Fimages%2Fground%2Fground.2.png&w=256&q=75") repeat-x left top'
           }
           backgroundSize={'64px 128px'}
           overflow={'hidden'}
@@ -33,7 +33,7 @@ const Ground: FC<Props> = ({ x, height, width }: Props) => {
           w={'4px'}
           h={height + 'px'}
           bg={
-            'url("/_next/image?url=%2Fimages%2Fground%2Fground.3.png&w=128&q=80") no-repeat left top'
+            'url("/_next/image?url=%2Fimages%2Fground%2Fground.3.png&w=128&q=75") no-repeat left top'
           }
           backgroundSize={'4px 128px'}
           overflow={'hidden'}
