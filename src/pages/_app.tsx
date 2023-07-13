6d0098692b4088d8ed15d7bb805f4a1af033cf5d
engelde <davidengel@live.com>
2023-07-13 16:47:55 -0700

    Refactor store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
+import { useEffect } from 'react'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { StoreProvider } from '@/utilities/store'
+import { useStore } from '@/utilities/store'
 import { theme } from '@/utilities/theme'
 import '@fontsource/vt323/400.css'
 import '@/styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  // Hydrate store
+  useEffect(() => {
+    void useStore.persist.rehydrate()
+  }, [])
+
   return (
-    <StoreProvider>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </StoreProvider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
 
--- src/utilities/store.tsx
+++ src/utilities/store.tsx
@@ -1,54 +1,23 @@
-import { createContext, useContext, useReducer } from 'react'
+import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
 
-export type StoreContext = ReturnType<typeof useStoreValue>
-
-export type StoreState = typeof initialState
-
-export type StoreAction = {
-  type: string
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  payload?: any
+type State = {
+  audio: number
+  setAudio: (val: number) => void
 }
 
-const StoreContext = createContext<StoreContext | undefined>(undefined)
-StoreContext.displayName = 'StoreContext'
-
-const initialState = {
-  audio: 0,
-}
-
-const reducer = (state: StoreState, action: StoreAction) => {
-  switch (action.type) {
-    case 'FETCH_AUDIO':
-      state.audio = parseInt(
-        localStorage.getItem('audio') !== null ? (localStorage.getItem('audio') as string) : '0',
-        10,
-      )
-      return state
-
-    case 'UPDATE_AUDIO':
-      localStorage.setItem('audio', action.payload as string)
-      state.audio = action.payload as number
-      return state
-
-    default:
-      return state
-  }
-}
-
-const useStoreValue = () => {
-  return useReducer(reducer, initialState)
-}
-
-export const useStore = () => {
-  const value = useContext(StoreContext)
-  if (value === undefined) {
-    throw new Error('useStore must be used within a StoreProvider')
-  }
-  return value
-}
-
-export function StoreProvider<T>(props: T) {
-  const value = useStoreValue()
-  return <StoreContext.Provider value={value} {...props} />
-}
+export const useStore = create<State>()(
+  devtools(
+    persist(
+      (set) => ({
+        audio: 0,
+        setAudio: (val) => set(() => ({ audio: val })),
+      }),
+      {
+        name: 'app',
+        // Hydrate in _app.tsx to fix localstorage mismatch
+        skipHydration: true,
+      },
+    ),
+  ),
+)
