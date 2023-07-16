45bc844ac9f25ccc71da303274ff11fc8637233f
engelde <davidengel@live.com>
2023-07-16 12:06:49 -0700

    Simplify build

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-  // this will check Typescript files
   '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
 
-  // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `pnpm eslint --fix ${filenames.join(' ')}`,
     `pnpm prettier --write ${filenames.join(' ')}`,
   ],
 
-  // this will Format MarkDown and JSON
   '**/*.(md|json)': (filenames) => `pnpm prettier --write ${filenames.join(' ')}`,
 }
