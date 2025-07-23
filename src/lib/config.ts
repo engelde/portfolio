4a2fe97871571bb67cc645d67875671193a0f3e4
engelde <davidengel@live.com>
2025-07-22 22:49:52 -0700

    fix(bug): get version from package.json

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,6 @@ export const config = {
     author: process.env.NEXT_PUBLIC_APP_AUTHOR || '',
     description: process.env.NEXT_PUBLIC_APP_DESCRIPTION || '',
     version: process.env.NEXT_PUBLIC_APP_VERSION || '',
-    environment: process.env.NODE_ENV || '',
+    environment: process.env.version || '',
   },
 }
